f7a30270c5ce8d99cbdc1832c4c57f0835d96fda
faassen <faassen@startifact.com>
2016-09-19 16:45:00 +0200

    Document a few historical ways to deal with Morepath configuration.

--- morepath/app.py
+++ morepath/app.py
@@ -16,7 +16,15 @@ class Welcome(object):
 def root_default(self, request):
     return "Hello World!"
 
+# from version 0.4 until 0.12. 0.13 breaks this
+#config = morepath.setup()
+#config.scan()
+#config.commit()
 
+# 0.13
+# morepath.commit(App)
+
+# from 0.14
 App.commit()
 
 main = App()
